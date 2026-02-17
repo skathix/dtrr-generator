e7cc12efaf4967035387231bfcc1007d84aa3b66
AAndrews <aandrews@accesshealth.services>
2026-02-17 12:55:48 -0600

    WIP Misc updates, type 23, 24 MBIT names

--- obj/Debug/net6.0/FileIngestor.AssemblyInfo.cs
+++ obj/Debug/net6.0/FileIngestor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileIngestor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59581bcc2784cb3e59146a333d66d35cad5fdd6b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4cb0ae561d0b468e2b5e7b9de6f002840967ed66")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileIngestor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileIngestor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
