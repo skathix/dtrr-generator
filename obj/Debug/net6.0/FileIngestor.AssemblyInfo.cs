12e2e526ae4834f59ce4228f07250a92d79990bc
AAndrews <aandrews@accesshealth.services>
2026-06-01 11:38:07 -0500

    a

--- obj/Debug/net6.0/FileIngestor.AssemblyInfo.cs
+++ obj/Debug/net6.0/FileIngestor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileIngestor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59af1583d02dc3cb879d8543d70bd2291f14d55f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c62b29145d648dd690040edf4c8efb1b5581bfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileIngestor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileIngestor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
